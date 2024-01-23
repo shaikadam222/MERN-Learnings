1f9a618b4e380f0bca1c8bba9db368e317def82d
Shaik <shaikadam642@gmail.com>
2024-01-23 09:27:47 +0530

    23 Jan

--- todo_database/backend.js
+++ todo_database/backend.js
@@ -18,10 +18,11 @@ mongoose.connect(url, {
   
 const userschema = new mongoose.Schema({
     username : String,
-    password: String
+    password: String,
+    token: String
 })
 const todoScheme = new mongoose.Schema({
-    id: ObjectId,
+    id: Number,
     title: String,
     description: String,
     published : Boolean
@@ -30,65 +31,113 @@ const todo = mongoose.model('Todo',todoScheme,'Todos');
 const user = mongoose.model('User',userschema,'Users');
 
 /////////////////////////////////////////////////// USER STUFF
-let token;
 function authenticate(req,res,next) {
+    var str = " ";
     var obj = {
         username : req.body.username,
-        password : req.body.password
+        password : req.body.password,
+        token : str
     }
     // console.log(obj);
     var secretkey = "S3crEt";
-    token = jwt.sign(obj,secretkey)
+    req.token = jwt.sign(obj,secretkey)
+    obj.token = req.token
     // console.log(token);
     next();
 }
-app.post('/user/signup',authenticate,(req,res) => {
-    if(token == undefined)
+app.post('/user/signup',authenticate,async (req,res) => {
+    if(req.token == undefined)
     {
         res.sendStatus(403);
     }
     else
     {
-        var check = user.findOne({username : req.body.username})
-        console.log(check.username)
-        if(check != undefined)
+        var check = await user.findOne({username : req.body.username})
+        // console.log(check)
+        if(check)
+        {
+            console.log("found user" + check.username)
+            res.status(500).send("Username already exists")
+        }
+        else
         {
             const newuser = new user({
                 username : req.body.username,
-                password : token
+                password : req.body.password,
+                token : req.token
             })
             
             
             // if(check.getQuery(user.username))
-            newuser.save().then(()=> {
+            await newuser.save().then(()=> {
                 console.log("user created");
             }).catch((error) => {
                 console.log("error in saving");
             })
-    
             res.send(newuser);
-        }
-        else
-        {
-            console.log("found user" + check.username)
         }
           
         
 
     }
 })
+///////////////////////////////////////////////////  user login
+app.post('/user/login' ,async(req,res) => {
+    let check = await user.findOne({username: req.body.username})
+    if(check != undefined)
+    {
+        if(check.username == req.body.username && check.password === req.body.password)
+        {
+            return res.status(200).send("logged in successfully");
+        }
+        else
+        {
+            return res.status(401).send("invalid credentials");
+        }
+    }
+    else
+    {
+        res.status(403).send("username doesn't exits");
+    }
+})
 /////////////////////////////////////////////////// TODOS STUFFF
-app.post('/todos',(req,res) => {
+app.post('/user/todos',async (req,res) => {
     const newtodo = new todo({
+        id : req.body.id,
         title: req.body.title,
         description: req.body.description
     })
-    newtodo.save().then(() => {
+   await newtodo.save().then(() => {
         console.log("created ");
-    });
-
+        res.status(200).send(newtodo);
+    }); 
 })
+app.get('/user/todos',async(req,res) => {
+    let check = await  todo.find()
+    if(check == undefined)
+    {
+        res.status(403).send("no todos available");
+    }
+    else
+    {
+        res.status(200).send(check)
+    }
+})
+app.put('/user/todos/:id',async (req,res) => {
+    let check = await todo.find({id : req.params.id})
+    if(check == undefined)
+    {
+        res.status(404).send("Todo can't find");
+    }
+    else
+    {
+        check.title = req.body.title;
+        check.description = req.body.description;
 
+        console.log(check);
+        res.send(check);
+    }
+})
 app.listen(port,()=> {
     console.log("listening");
 })
