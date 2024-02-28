5b1e54bd2c2e96a35176876b60f903c375eb0195
Shaik <shaikadam642@gmail.com>
2024-02-28 18:15:58 +0530

    REACT kearnings

--- REACT/my-react-app/src/index.js
+++ REACT/my-react-app/src/index.js
@@ -4,10 +4,19 @@ import APP2 from "./components/APP2";
 
 ReactDOM.render(<APP2 />, document.getElementById("root"));
 
-//1. Apply CSS styles to App.jsx component
-//to match the appearance on the completed app:
-//https://c6fkx.csb.app/
-//2. Extract the contact card as a reusable Card component.
-//3. Use props to render the default Beyonce contact card
-//so the Card component can be reused for other contacts.
-//4. Import the contacts.js file to create card components.
+// //1. Apply CSS styles to App.jsx component
+// //to match the appearance on the completed app:
+// //https://c6fkx.csb.app/
+// //2. Extract the contact card as a reusable Card component.
+// //3. Use props to render the default Beyonce contact card
+// //so the Card component can be reused for other contacts.
+// //4. Import the contacts.js file to create card components.
+
+
+// import emojipedia from "./emojipedia";
+// // console.log(emojipedia)
+
+// const newemojis = emojipedia.map((emojipedia) => {
+//     return (emojipedia.meaning).substring(0,100);
+// });
+// console.log(newemojis)
