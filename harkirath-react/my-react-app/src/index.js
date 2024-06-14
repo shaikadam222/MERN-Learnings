d82c17ec2db8287a72a38e1acbedfa75321416c4
adam <shaikadam642@gmail.com>
2024-06-14 10:33:34 +0530

    update from new OS

--- harkirath-react/my-react-app/src/index.js
+++ harkirath-react/my-react-app/src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+ReactDOM.render(
+    <div>
+        <App/>
+    </div>
+,document.getElementById('root'))
 
 
 
